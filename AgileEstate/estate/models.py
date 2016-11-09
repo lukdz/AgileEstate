73e8a10e2e459484fe9bd230d137c6fb96167775
ref-humbold <rafkaleta@onet.pl>
2016-11-09 13:57:39 +0100

    Uproszczona wersja modelu nieruchomości z wstępnymi testami.

--- AgileEstate/estate/models.py
+++ AgileEstate/estate/models.py
@@ -2,4 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class PropertyModel(models.Model):
+    country = models.TextField()
+    city = models.TextField()
+    surface = models.DecimalField(decimal_places=2)
+    minimal_price = models.DecimalField(decimal_places=2)
+    
+    def save(self, *args, **kwargs):
+        if self.surface <= 0:
+            raise TypeError("Property surface is less than or equal to zero.")
+        
+        if self.minimal_price <= 0:
+            raise TypeError("Property minimal_price is less than or equal to zero.")
+
--- AgileEstate/estate/tests.py
+++ AgileEstate/estate/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import PropertyModel
+
+class PropertyModelTestCase(TestCase):
+    def setUp(self):
+        model = PropertyModel(country="Country", city="City")
+    
+    def tearDown(self):
+        pass
+    
+    def test_save_when_surface_is_less_than_zero(self):
+        model.surface = -123.45
+        self.assertRaises(TypeError, model.save())
+    
+    def test_save_when_surface_equals_zero(self):
+        model.surface = 0.0
+        self.assertRaises(TypeError, model.save())
+        
+    def test_save_when_minimal_price_is_less_than_zero(self):
+        model.minimal_price = -123.45
+        self.assertRaises(TypeError, model.save())
+    
+    def test_save_when_minimal_price_equals_zero(self):
+        model.minimal_price = 0.0
+        self.assertRaises(TypeError, model.save())
+
