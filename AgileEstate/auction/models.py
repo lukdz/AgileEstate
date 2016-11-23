2fc8a560ce706623f49bb4b3b3e004e693ca4343
ref-humbold <rafkaleta@onet.pl>
2016-11-23 13:55:20 +0100

    Rozszerzenie modeli w auction i estate

--- AgileEstate/auction/models.py
+++ AgileEstate/auction/models.py
@@ -1,16 +1,29 @@
-from django.db import models
-
+from __future__ import unicode_literals
+from datetime import *
 from decimal import *
 
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.timezone import utc
+
 class BiddingModel(models.Model):
-    property_key = models.ForeignKey('PropertyModel', models.CASCADE)
-    owner_key = models.ForeignKey('Users', models.CASCADE)
+    property_key = models.ForeignKey('estate.PropertyModel', models.CASCADE)
+    owner_key = models.ForeignKey('users.CumstomUser', models.CASCADE)
+    winner_key = models.ForeignKey('users.CumstomUser', models.CASCADE)
     
-    minimal_price = models.DecimalField(max_digits=1002, decimal_places=2, default=0.0, validators=[ MinValueValidator(0.0) ] )
-    actual_price = models.DecimalField(max_digits=1002, decimal_places=2, default=minimal_price, validators=[ MinValueValidator(minimal_price) ] )
+    start_time = models.DateTimeField( default=datetime.utcnow().replace(tzinfo=utc) )
+    end_time = models.DateTimeField( default=datetime.utcnow().replace(tzinfo=utc), validators=[ MinValueValidator(start_time) ] )
+    minimal_price = models.DecimalField( max_digits=1002, decimal_places=2, default=0.0, validators=[ MinValueValidator(0.0) ] )
+    actual_price = models.DecimalField( max_digits=1002, decimal_places=2, default=minimal_price, validators=[ MinValueValidator(minimal_price) ] )
     
-    def set_new_actual_price(self, new_price):
+    def set_new_actual_price(self, new_price, time):
+        if not self.is_bid_open(time):
+            raise SystemError("Cannot bid when auction is not open.")
+        
         if new_price <= self.actual_price:
             raise AttributeError("New price is less than actual price.")
-        
+    
         self.self.actual_price = new_price
+    
+    def is_bid_open(self, time):
+        return start_time <= time <= end_time
--- AgileEstate/auction/tests.py
+++ AgileEstate/auction/tests.py
@@ -1,20 +1,23 @@
-from django.test import TestCase
-
-from .models import BiddingsModel
-
+from datetime import *
 from decimal import *
+
+from django.test import TestCase
+from .models import BiddingsModel
 
 class BiddingModelTestCase(TestCase):
     def setUp(self):
         getcontext().prec = 2
-        model = BiddingModel( minimal_price=Decimal(100.00), actual_price=Decimal(200.00) )
+        model = BiddingModel( start_time=datetime(2001, 2, 3, 4, 5, 6), end_time=datetime(2002, 10, 11, 12, 13, 14), end_time=datetime(), minimal_price=Decimal(100.00), actual_price=Decimal(200.00) )
     
     def tearDown(self):
         pass
     
-    def test_set_new_actual_price_error(self):
-        self.assertRaises(AttributeError, model.set_new_actual_price( Decimal(190.00) ))
+    def test_set_new_actual_price_error_1(self):
+        self.assertRaises( SystemError, model.set_new_actual_price( Decimal(210.00), datetime(2003, 4, 5, 6, 7, 8) ) )
+    
+    def test_set_new_actual_price_error_2(self):
+        self.assertRaises( AttributeError, model.set_new_actual_price( Decimal(190.00), datetime(2001, 7, 8, 9, 10, 11) ) )
     
     def test_set_new_actual_price_correct(self):
-        model.set_new_actual_price( Decimal(190.00) )
+        model.set_new_actual_price( Decimal(210.00), datetime(2001, 7, 8, 9, 10, 11) )
 
