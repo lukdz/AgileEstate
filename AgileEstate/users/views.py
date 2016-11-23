aab0561fac19eef510dd3bec6a1ce71f43e4ebb3
Chleb <karol.bochenski.1995@gmail.com>
2016-11-23 11:31:29 +0100

    Dodano stronę logowania i rejestracji

--- AgileEstate/users/views.py
+++ AgileEstate/users/views.py
@@ -1,3 +1,58 @@
+<<<<<<< HEAD
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.shortcuts import render
+from .forms import UserForm
+# Create your views here.
+
+def index(request):
+    if not request.user.is_authenticated():
+        return render(request, 'profile/base.html')
+
+
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'profile/index.html')
+    context = {
+        "form": form,
+    }
+    return render(request, 'profile/register.html', context)
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'profile/login.html', context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'profile/index.html')
+            else:
+                return render(request, 'profile/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'profile/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'profile/login.html')
+=======
 from django.shortcuts import render
 
 # Create your views here.
+>>>>>>> 13d921fcac8bd18407f0333f24464b4a7cccc6b6
