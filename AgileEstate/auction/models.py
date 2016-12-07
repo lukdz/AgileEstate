377ab21b8f6d785fe8ed18334d0dc133fb95ba18
ref-humbold <rafkaleta@onet.pl>
2016-12-07 13:02:54 +0100

    Poprawa modeli w auction i estate. Dodanie funckjonalności współrzędnych geograficznych.

--- AgileEstate/auction/models.py
+++ AgileEstate/auction/models.py
@@ -3,16 +3,16 @@ from datetime import *
 from decimal import *
 
 from django.db import models
+from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.utils.timezone import utc
 
 class BiddingModel(models.Model):
     property_key = models.OneToOneField('estate.PropertyModel', models.CASCADE)
-    owner_key = models.OneToOneField('users.CustomUser', models.CASCADE)
-    #winner_key = models.OneToOneField('users.CustomUser', models.CASCADE)
+    owner_key = models.OneToOneField('users.UserProfile', on_delete=models.CASCADE)
+    #winner_key = models.OneToOneField('users.UserProfile', on_delete=models.CASCADE)
     
-    start_time = models.DateTimeField( default=datetime.utcnow().replace(tzinfo=utc) )
-    end_time = models.DateTimeField( default=datetime.utcnow().replace(tzinfo=utc), validators=[ MinValueValidator(start_time) ] )
+    start_time = models.DateTimeField( default=timezone.now() )
+    end_time = models.DateTimeField( default=timezone.now(), validators=[ MinValueValidator(start_time) ] )
     minimal_price = models.DecimalField( max_digits=902, decimal_places=2, default=0.0, validators=[ MinValueValidator(0.0) ] )
     actual_price = models.DecimalField( max_digits=902, decimal_places=2, default=minimal_price, validators=[ MinValueValidator(minimal_price) ] )
     
@@ -22,7 +22,7 @@ class BiddingModel(models.Model):
         
         if new_price <= self.actual_price:
             raise AttributeError("New price is less than actual price.")
-    
+        
         self.actual_price = new_price
     
     def is_bid_open(self, time):
