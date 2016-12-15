0f52d9f1a1fc5a9a3cf57a6bced04b7d1fa521f5
lukdz <kul1994@o2.pl>
2016-12-15 14:12:50 +0000

    allowed hosts

--- AgileEstate/AgileEstate/settings.py
+++ AgileEstate/AgileEstate/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g6zkhlltk6-ffjl8xddb!tz8(y$$dgq=&phevu!dh(d3db$bf!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
