cb0e1ad82086dd22f20af17a5111566534734f27
ref-humbold <rafkaleta@onet.pl>
2017-01-04 13:59:31 +0100

    Property form view

--- AgileEstate/estate/views.py
+++ AgileEstate/estate/views.py
@@ -3,9 +3,9 @@ from django.views.generic.edit import FormView
 from .forms import PropertyForm
 
 class PropertyFormView(FormView):
-    template_name = "propertyForm.html"
+    template_name = "add-property.html"
     form_class = PropertyForm
-    success_url = "/add_property/"
+    success_url = "/success/"
 
     def form_valid(self, form):
         return super(PropertyFormView, self).form_valid(form)
